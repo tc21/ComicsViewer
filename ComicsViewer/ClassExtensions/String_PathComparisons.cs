2e9a983a0c590f13e6e1dce4052930a54f532d44
Tianyi Cao <tc21@live.com>
2020-08-17 20:42:43 -0700

    Fixed a bug caused by an incorrect implementation of GetPathRelativeTo where dropping a category folder into the app would throw an exception.

--- ComicsViewer/ClassExtensions/String_PathComparisons.cs
+++ ComicsViewer/ClassExtensions/String_PathComparisons.cs
@@ -9,20 +9,33 @@ using System.Threading.Tasks;
 
 namespace ComicsViewer.ClassExtensions {
     public static class String_PathComparisons {
+        /// <summary>
+        /// Tests whether <c>child</c> is <c>parent</c> or a subdirectory in <c>parent</c>.
+        /// </summary>
         public static bool IsChildOfDirectory(this string child, string parent) {
             return Path.GetFullPath(child).StartsWith(Path.GetFullPath(parent), StringComparison.OrdinalIgnoreCase);
         }
 
+        /// <summary>
+        /// <list type="table">
+        /// <item>If <c>path</c> is a subdirectory of <c>parent</c>, returns the relative path of <c>path</c> to <c>parent</c>.</item>
+        /// <item>If <c>path</c> is <c>parent</c>, returns an empty string.</item>
+        /// <item>Otherwise, returns the absolute path of <c>path</c>.</item>
+        /// </list>
+        /// </summary>
         public static string GetPathRelativeTo(this string path, string parent) {
+            path = Path.GetFullPath(path);
+            parent = Path.GetFullPath(parent);
+
             if (!path.IsChildOfDirectory(parent)) {
                 return path;
             }
 
-            if (path == parent) {
+            if (path.Equals(parent, StringComparison.OrdinalIgnoreCase)) {
                 return "";
             }
 
-            return Path.GetFullPath(path).Substring(Path.GetFullPath(parent).Length + 1);
+            return path.Substring(parent.Length + 1);
         }
     }
 }
