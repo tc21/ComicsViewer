d3190f8d93bd81d2ae5e2038ffdfa13885669d8c
Tianyi Cao <tc21@live.com>
2020-10-11 12:27:49 -0700

    Fixed release-mode crashes when calling StrCmpLogicalW

--- ComicsViewer.Common/NaturalOrder.cs
+++ ComicsViewer.Common/NaturalOrder.cs
@@ -3,7 +3,7 @@ using System.Runtime.InteropServices;
 
 namespace ComicsViewer.Common {
     public static class NaturalOrder {
-        [DllImport("shlwapi.dll", CharSet = CharSet.Unicode)]
+        [DllImport("shlwapi.dll", CharSet = CharSet.Unicode, ExactSpelling = true)]
         private static extern int StrCmpLogicalW(string psz1, string psz2);
         public static readonly IComparer<string> Comparer = new NaturalOrderComparer();
 
